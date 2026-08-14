2e6f20273258b459487da0882b8e03e4642b348f
Vinu K <kevy.vinu@gmail.com>
2026-08-14 08:43:54 +0000

    multi-module: svc-a vuln (v0.23.0), svc-b patched (v0.33.0) for CVE-2024-45338
    Signed-off-by: Vinu K <kevy.vinu@gmail.com>

---
+++ svc-a/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(strings.NewReader("<html><body><p>svc-a</p></body></html>"))
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println("parsed:", doc.Type)
+}
