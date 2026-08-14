694854fd524ce033c81529acbe436d1b27730fdc
Vinu K <kevy.vinu@gmail.com>
2026-08-14 10:09:45 +0000

    vuln-replace-directive: add x/crypto/ssh for CVE-2024-45337 (both deps vulnerable, x/net replaced)
    Signed-off-by: Vinu K <kevy.vinu@gmail.com>

--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module github.com/k37y/gvs-testdata
 
 go 1.22.0
 
-require golang.org/x/net v0.23.0
+require (
+	golang.org/x/crypto v0.23.0
+	golang.org/x/net v0.23.0
+)
+
+require golang.org/x/sys v0.20.0 // indirect
 
 replace golang.org/x/net => golang.org/x/net v0.24.0
--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"strings"
 
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/net/html"
 )
 
 func main() {
+	// CVE-2024-45338: golang.org/x/net/html.Parse
 	doc, err := html.Parse(strings.NewReader("<html><body><p>hello</p></body></html>"))
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("html parse error:", err)
 		return
 	}
 	fmt.Println("parsed:", doc.Type)
+
+	// CVE-2024-45337: golang.org/x/crypto/ssh.NewServerConn
+	config := &ssh.ServerConfig{}
+	conn, _ := net.Pipe()
+	_, _, _, _ = ssh.NewServerConn(conn, config)
 }
